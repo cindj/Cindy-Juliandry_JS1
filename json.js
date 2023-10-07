e1ab14eab8054f8255b2f7e61a7e8598bc084b61
Cindy J <cindyj2507@gmail.com>
2023-10-08 03:18:02 +0700

    CindyJuliandry_JS1
    CindyJuliandry_JS1

---
+++ json.js
@@ -0,0 +1,81 @@
+// // Function If
+// let a = 1
+// if(a <= 15){
+//     a++;
+//     console.log(a);
+// }
+
+// // // Function If .. Else
+// let b = 10
+// if(b === 10){
+//     console.log("Ini adalah angka 10")
+// } else {
+//     console.log("Ini bukan angka 10")
+// }
+
+// // // Function If .. Else (2)
+// let d = 11
+// if(d === 10){
+//     console.log("Ini adalah angka 10")
+// } else {
+//     console.log("Ini bukan angka 10")
+// }
+
+// // // Nested If
+// let x = 7
+// let y = 10
+// if (x > 6){
+//  console.log("Nomor X lebih besar dari 6");
+//      if(y > 6){
+//          console.log("Nomor Y lebih besar dari 6");
+//      } else {
+//          console.log("Nomor Y lebih kecil dari 6")
+//      }        
+// } else{
+//      console.log("Nomor X lebih kecil dari 6")   
+// }
+
+// // // Switch
+// let gender = "female"
+// switch (gender){
+//      case "female":
+//           console.log("These are female");
+//           break;
+//      case "male":
+//           console.log("These are male");
+//           break;
+//      default:
+//           console.log("Unidentified gender");
+// }
+
+// // // For
+// for (let c = 0; c < 5; c++){
+//      console.log(c);
+// }
+
+// // // While
+// let j = 3
+// while (j <= 10){
+//     console.log(x)
+//     j++;
+// }
+
+// // // While  Do
+// let e = 3
+// do {
+//  console.log(e);
+//  e++;
+// }while(e <= 10);
+
+
+// // // Function 
+// let fun = math(3,12)
+// function math(a,b){
+//     console.log(b*a);
+// }
+
+// // // Function (2)
+// const test= (a,b) => {
+//     console.log(a/b);
+// }
+// fun = test(30,6)
